3b7551ecc95a8fea7d94c90034f601795e7e12ce
AURORA GONZALEZ VIDAL <aurora.gonzalez2@um.es>
2019-10-10 20:14:55 +0200

    cambios

--- R/streamQsimplified.R
+++ R/streamQsimplified.R
@@ -1,6 +1,6 @@
 #library("tsoutliers")
 
-streamQ <- function(x) {
+streamQsimplified <- function(x) {
   dat.ts<- ts(x,frequency=1)
   data.ts.outliers <- tso(dat.ts, types = c("AO", "LS", "TC", "IO", "SLS"))
   Q = 1 - nrow(data.ts.outliers$outliers)/nrow(xy)
